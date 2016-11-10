a35e4cc6f5e18f79c44ceaccc14c0148a21aa462
Pramod Singh <pramod.singh@kiwitech.com>
2016-11-10 13:38:45 +0530

    cookbook name modified

--- kt_lampstack/metadata.rb
+++ kt_lampstack/metadata.rb
@@ -1,7 +1,7 @@
-name             'lampstack'
+name             'kt_lampstack'
 maintainer       'KiwiTech'
 maintainer_email 'guddu.khan@kiwitech.com'
 license          'All rights reserved'
 description      'Installs/Configures lampstack'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
