8866daaabd0bc459538b7b432d392aca8e99d8d2
Pramod Singh <pramod.singh@kiwitech.com>
2016-11-19 16:48:35 +0530

    syntax issue fixed with bash resource

--- php_custom/recipes/configure.rb
+++ php_custom/recipes/configure.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
   end
  
   #write out app.php in to the app
-  template "#{deploy[:absolute_document_root]}/config/app.php" do
+  template "#{deploy[:absolute_document_root]}config/app.php" do
     source 'app.php.erb'
     mode '0660'
     owner deploy[:user]
@@ -22,7 +22,7 @@ bash 'Fix permissions' do
   code <<-EOH
     find #{deploy[:absolute_document_root]} -type d -exec chmod 755 {} \\;
     find #{deploy[:absolute_document_root]} -type f -exec chmod 644 {} \\;
-    chmod -R 0777 #{deploy[:absolute_document_root]}/tmp #{deploy[:absolute_document_root]}/logs
+    chmod -R 0777 #{deploy[:absolute_document_root]}tmp #{deploy[:absolute_document_root]}logs
     EOH
   only_if do
     File.exists?("#{deploy[:absolute_document_root]}")
@@ -30,22 +30,22 @@ bash 'Fix permissions' do
 end
 
 #Install composer and run composer.phar install 
-  cookbook_file "#{deploy[:absolute_document_root]}/composer-setup.php" do
+  cookbook_file "#{deploy[:absolute_document_root]}composer-setup.php" do
     source 'composer-setup.php'
     mode '0755'
     owner deploy[:user]
     group deploy[:group]
     not_if do
-      File.exists?("#{deploy[:absolute_document_root]}/composer-setup.php")
+      File.exists?("#{deploy[:absolute_document_root]}composer-setup.php")
     end  
   end
   bash 'php composer-setup.php' do
-    code "php #{deploy[:absolute_document_root]}/composer-setup.php"
+    code "php #{deploy[:absolute_document_root]}composer-setup.php"
   end
-  bash './composer.phar install' do
-    code "#{deploy[:absolute_document_root]}/composer.phar install"
+  bash 'composer.phar install' do
+    code "#{deploy[:absolute_document_root]}composer.phar install"
     only_if do
-      File.exists?("deploy[:absolute_document_root]}/composer.lock") 
+      File.exists?("#{deploy[:absolute_document_root]}composer.lock") 
     end
   end
 end
